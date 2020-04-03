0bb2eebc9171b07bf3ccd557460c0ede9bb39d89
Jacob Cramer <jacobdanielcramer@gmail.com>
2020-04-02 23:36:05 -0400

    Initial commit of files for github upload

---
+++ precache-manifest.c29008b2543f6399a221afab28958535.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee2f388799efe8c06a87d50d7a80fd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f1928fb8b727ed580a",
+    "url": "/static/css/main.d165e617.chunk.css"
+  },
+  {
+    "revision": "8bfdb5891135fe5012d0",
+    "url": "/static/js/2.52d59eb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.52d59eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f1928fb8b727ed580a",
+    "url": "/static/js/main.2e24ee7a.chunk.js"
+  },
+  {
+    "revision": "ca4a2c76173b71d8401d",
+    "url": "/static/js/runtime-main.36c35303.js"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/static/media/delete.aeec2863.svg"
+  },
+  {
+    "revision": "5ad085c32b0596f4a58e11f44288ddbe",
+    "url": "/static/media/move.5ad085c3.svg"
+  }
+]);
